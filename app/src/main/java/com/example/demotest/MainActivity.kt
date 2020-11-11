5e5656377693ae753c341d8722c28675816dd95e
王嘉杰 <wangjiajie@visioninsight.com>
2020-11-11 11:40:37 +0800

    123

--- app/src/main/java/com/example/demotest/MainActivity.kt
+++ app/src/main/java/com/example/demotest/MainActivity.kt
@@ -3,18 +3,18 @@ package com.example.demotest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.demotest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var databind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("TAG", "onCreate: ")
-        print("23")
-        print("123")
-        print("测试")
-        print("分支测试")
-        print("测试2")
-        print("测试3")
-        print("分支测试3")
+
+        databind = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/demotest/Test.kt
+++
@@ -1,3 +0,0 @@
-package com.example.demotest
-
-data class Test(val name: String)
